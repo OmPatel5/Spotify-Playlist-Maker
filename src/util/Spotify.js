f431ad848107b125effab769c177a241a0d69039
OmPatel5 <omp091216@gmail.com>
2022-12-21 20:52:24 -0500

    first commit

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -37,19 +37,20 @@ let Spotify = {
             window.location.href = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=token&redirect_uri=${redirectURI}`
             let url = window.location.href;
             access_token = url.match('access_token=([^&]*)')[1];
-            let baseURL = 'https://api.spotify.com';
-            let searchParam = `/v1/search?type=track&q=d`;
-            let endpoint = baseURL + searchParam;
-            console.log(access_token)
+            return access_token;
+            // let baseURL = 'https://api.spotify.com';
+            // let searchParam = `/v1/search?type=track&q=d`;
+            // let endpoint = baseURL + searchParam;
+            // console.log(access_token)
 
-            const response = await fetch(endpoint, {
-                headers: {
-                    Authorization: `Bearer ${access_token}`,
-                    "Content-Type": 'application/json'
-                }
-            });
+            // const response = await fetch(endpoint, {
+            //     headers: {
+            //         Authorization: `Bearer ${access_token}`,
+            //         "Content-Type": 'application/json'
+            //     }
+            // });
             
-            console.log(response.json())
+            // console.log(response.json())
         }
         
     },
